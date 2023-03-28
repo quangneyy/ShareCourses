168dcc90633f9ae819c32ecca2f3f58b82adbec8
Quang Ney <quangney12602@gmail.com>
2023-03-29 02:16:29 +0700

    Add project files.

---
+++ ShareCourses/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShareCourses.Startup))]
+namespace ShareCourses
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
