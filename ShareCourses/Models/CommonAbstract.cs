d765b8aa6fc11741e18614e3b3da91d8c9769324
Quang Ney <quangney12602@gmail.com>
2023-03-30 01:26:59 +0700

    Model

---
+++ ShareCourses/Models/CommonAbstract.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShareCourses.Models
+{
+    public class CommonAbstract
+    {
+        public string CreateBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedrDate { get; set; }
+        public string ModifiedBy{ get; set; }
+    }
+}
