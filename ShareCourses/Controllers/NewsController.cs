34a119afcee41c116870013ed8a7f07679197214
Quang Ney <quangney12602@gmail.com>
2023-04-03 13:34:07 +0700

    commit-sencond

---
+++ ShareCourses/Controllers/NewsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ShareCourses.Models;
+
+namespace ShareCourses.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: 
+        public ActionResult Index(int? page)
+        {
+            var pageSize = 10;
+            if (page == null)
+            {
+                page = 1;
+            }
+            IEnumerable<News> items = db.News.OrderByDescending(x => x.CreatedDate);
+            var pageIndex = page.HasValue ? Convert.ToInt32(page) : 1;
+            items = items.ToPagedList(pageIndex, pageSize);
+            ViewBag.PageSize = pageSize;
+            ViewBag.Page = page;
+            return View(items);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var item = db.News.Find(id);
+            return View(item);
+        }
+
+        public ActionResult Partical_News_Home()
+        {
+            var items = db.News.Take(3).ToList();
+            return ParticalView(items);
+        }
+    }
+}
